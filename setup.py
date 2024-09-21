9a8695297bfbf07b4e831067ab0005db83c35b59
DavyCW <theseus329@gmail.com>
2024-09-21 00:00:39 +0000

    with working git and pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""setup.py file for turning on setup.cfg file.
+
+File: setup.py
+
+This module contains the setup configuration for break_projects.
+It uses setuptools to handle the package distribution and installation.
+"""
+
+from setuptools import setup
+
+setup()
